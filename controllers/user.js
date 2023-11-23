174125373fdfb7b797629fc1f1d1ecb600ff54b8
majoh19 <maria.hernandezr@est.iudigital.edu.co>
2023-11-23 00:37:03 -0500

    :sparkles:validateJWT

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,8 @@
 const User = require('../models/user')
-const {request,response} = require('express')
-const {validationResult, check} = require('express-validator')
+const { request, response } = require('express')
+const { validationResult, check } = require('express-validator')
 const bycript = require('bcryptjs')
+const { validateJWT } = require('../middleware/validateJWT')
 
 //Create user
 const createUser = async (req = request, res = response) => {
@@ -13,20 +14,22 @@ const createUser = async (req = request, res = response) => {
         ])
         const errors = validationResult(req)
         if (!errors.isEmpty) {
-            return res.status(400).json({message: errors.array()})
+            return res.status(400).json({ message: errors.array() })
         }
-        const existUser = await User.findOne({email: req.body.email})
+        const existUser = await User.findOne({ email: req.body.email })
         if (existUser) {
             return res.status(400).send('The user already exists')
         }
-        let user = new User()
-        user.name = req.body.name
-        user.email = req.body.email
-        const salt = bycript.genSaltSync()
-        const password = bycript.hashSync(req.body.password, salt)
-        user.password = password
-        user = await user.save()
-        res.send(user)
+        validateJWT(req, res, async () => {
+            let user = new User()
+            user.name = req.body.name
+            user.email = req.body.email
+            const salt = bycript.genSaltSync()
+            const password = bycript.hashSync(req.body.password, salt)
+            user.password = password
+            user = await user.save()
+            res.send(user)
+        })
     } catch (error) {
         console.log(error)
         res.status(500).send('An error occured while creating the user')
@@ -36,8 +39,10 @@ const createUser = async (req = request, res = response) => {
 //List users
 const listUsers = async (req = request, res = response) => {
     try {
-        const users = await User.find()
-        res.send(users)
+        validateJWT(req, res, async () => {
+            const users = await User.find()
+            res.send(users)
+        })
     } catch (error) {
         console.log(error)
         res.status(500).send('An error occured while listing the users')
@@ -47,15 +52,17 @@ const listUsers = async (req = request, res = response) => {
 //Edit user
 const editUser = async (req = request, res = response) => {
     try {
-       const userId = req.params.userId
-       const user = await User.findById(userId)
-       if (!user) {
-        return res.status(400).send('User nor found')
-       }
-       if (req.body.name) user.name = req.body.name
-       if (req.body.email) user.email = req.body.email
-       const updatedUser = await user.save()
-       res.send(updatedUser)
+        validateJWT(req, res, async () => {
+            const userId = req.params.userId
+            const user = await User.findById(userId)
+            if (!user) {
+                return res.status(400).send('User not found')
+            }
+            if (req.body.name) user.name = req.body.name
+            if (req.body.email) user.email = req.body.email
+            const updatedUser = await user.save()
+            res.send(updatedUser)
+        })
     } catch (error) {
         console.log(error)
         res.status(500).send('An error occured while editing the user')
@@ -65,17 +72,19 @@ const editUser = async (req = request, res = response) => {
 //Delete user
 const deleteUser = async (req = request, res = response) => {
     try {
-        const userId = req.params.userId
-        const user = await User.findById(userId)
-        if (!user) {
-            return res.status(404).send('User not found')
-        }
-        await User.findByIdAndDelete(userId)
-        res.send('User deleted successfully')
+        validateJWT(req, res, async () => {
+            const userId = req.params.userId
+            const user = await User.findById(userId)
+            if (!user) {
+                return res.status(404).send('User not found')
+            }
+            await User.findByIdAndDelete(userId)
+            res.send('User deleted successfully')
+        })
     } catch (error) {
         console.log(error)
         res.status(500).send('An error occured while deleting the user')
     }
 }
 
-module.exports = {createUser, listUsers, editUser, deleteUser}
+module.exports = { createUser, listUsers, editUser, deleteUser }
